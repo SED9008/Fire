1788632955e5f2b7876bb2f81c53789133c08a9c
Leo <leoveenstra@gmail.com>
2015-11-12 14:51:26 +0100

    removed some old commented code

--- main.py
+++ main.py
@@ -9,21 +9,6 @@ from VideoManager import VideoManager
 print('OpenCV version:', cv2.__version__)
 
 
-# # Video acquisition
-# # Add video names
-# videos = {
-# 			'rgb2':''
-# 			,'swir':''
-# 			}
-
-# for video in videos:
-# 	videos[video] = cv2.VideoCapture(video + '.mp4')
-
-
-
-# ret, frame = videos['rgb2'].read()
-
-
 vm = VideoManager()
 
 vm.addStream('rgb2')
@@ -33,19 +18,6 @@ frames = vm.getFrames()
 for frame in frames:
 	cv2.imshow(frame,frames[frame])
 	cv2.waitKey(0)
-	
-
-# while(1):
-# 	for video in videos:	
-# 		# Capture frame-by-frame
-# 	    ret, frame = videos[video].read()
-
-# 	    # Display the resulting frame
-# 	    cv2.imshow('frame',frame)
-# 	    if cv2.waitKey(1) & 0xFF == ord('q'):
-# 	        break
-
-# # When everything done, release the capture
 
 vm.close()
 cv2.destroyAllWindows()
