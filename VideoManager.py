f54887c8fc565a539210edb26b6116b5372a6824
Leo <leoveenstra@gmail.com>
2015-11-12 22:32:19 +0100

    Minor changes

--- VideoManager.py
+++ VideoManager.py
@@ -4,15 +4,15 @@ class VideoManager:
 	def __init__(self):
 		self.videos = {}
 
-	def addStream(self, name):
-		self.videos[name] = cv2.VideoCapture(name + '.mp4')
+	def addStream(self, name, filename):
+		self.videos[name] = cv2.VideoCapture(filename)
 
 	def getFrame(self, video):
 		ret, frame = self.videos[video].read()
 		if not ret:
 			return False
 		return frame
-		
+
 	def getFrames(self):
 		frames = {}
 		for video in self.videos:
