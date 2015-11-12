b4482cbab14be956d87235aa0ef3df95fad2f332
Leo <leoveenstra@gmail.com>
2015-11-12 15:00:06 +0100

    Added getFrame()

--- VideoManager.py
+++ VideoManager.py
@@ -7,12 +7,19 @@ class VideoManager:
 	def addStream(self, name):
 		self.videos[name] = cv2.VideoCapture(name + '.mp4')
 
+	def getFrame(self, video):
+		ret, frame = self.videos[video].read()
+		if not ret:
+			return False
+		return frame
+		
 	def getFrames(self):
 		frames = {}
 		for video in self.videos:
 			ret, frames[video] = self.videos[video].read()
 			if not ret:
 				print('Error in retreiving frame from:', video)
+				return False
 		return frames
 
 	def close(self):
