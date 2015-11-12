03e65ff5cf074fef7e6465c6704fcf83808af9f1
Leo <leoveenstra@gmail.com>
2015-11-12 13:44:21 +0100

    video test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import cv2
 import pprint
 
-print "Hello world"
+
+print("OpenCV version:", cv2.__version__)
+
+videos["rgb"] = cv2.VideoCapture("rgb.mp4")
